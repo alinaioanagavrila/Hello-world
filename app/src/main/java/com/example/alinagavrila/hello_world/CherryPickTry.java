c7c195290385eadec0f69752d8af12168fa464ac
Alina Gavrila <alina.gavrila@softvision.ro>
2018-08-13 17:52:31 +0300

    CherryPick example

---
+++ app/src/main/java/com/example/alinagavrila/hello_world/CherryPickTry.java
@@ -0,0 +1,9 @@
+package com.example.alinagavrila.hello_world;
+
+public class CherryPickTry {
+
+    public static void main  (String[] args)
+    {
+        System.out.println("Cherry pick example");
+    }
+}
