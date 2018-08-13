5bb47b8f93de2a5571fb37aed264841668068bd8
Alina Gavrila <alina.gavrila@softvision.ro>
2018-08-13 17:42:24 +0300

    whatever

--- app/src/main/java/com/example/alinagavrila/hello_world/SecondClassExtended.java
+++ app/src/main/java/com/example/alinagavrila/hello_world/SecondClassExtended.java
@@ -2,38 +2,7 @@ package com.example.alinagavrila.hello_world;
 
 public class SecondClassExtended extends SecondClass {
 
-    public static void Number(int Nr){
-        switch (Nr){
-            case 1:  System.out.println ("One");
-                     break;
-            case 2:  System.out.println ("Two");
-                     break;
-            case 3:  System.out.println ("Three");
-                     break;
-            case 4:  System.out.println ("Four");
-                     break;
-            case 5:  System.out.println ("Five");
-                     break;
-            case 6:  System.out.println ("Six");
-                     break;
-            case 7:  System.out.println ("Seven");
-                     break;
-            case 8:  System.out.println ("eight");
-                     break;
-            case 9:  System.out.println ("Nine");
-                     break;
-            default: System.out.println ("I don't know what number is this!!!");
-                     break;
-        }
-    }
     public static void main (String[] args){
         System.out.println (MakeADream("Travel to Mexic"));
-        for (int i=0; i<13;i++)
-        {
-            if(i%2 == 0)
-                Number(i);
-            else
-                System.out.println ("Random stuff");
-        }
     }
 }
