a406e1eaca9bba9f283d03c84efc02796ff7c485
Alina Gavrila <alina.gavrila@softvision.ro>
2018-08-14 09:07:39 +0300

    Comment

--- app/src/main/java/com/example/alinagavrila/hello_world/HelloWorld.java
+++ app/src/main/java/com/example/alinagavrila/hello_world/HelloWorld.java
@@ -3,6 +3,6 @@ package com.example.alinagavrila.hello_world;
 public class HelloWorld {
 
     public static void main (String[] args) {
-        System.out.println("Hello world program modificat");
+        System.out.println("Hello World program modificat");
     }
 }
